1a0d9fd322178c051275e02aca689b8e2f5946be
mdcrodrigo <mdcrodrigo@hotmail.com>
2022-02-11 12:23:10 -0300

    Home, List, Card, Nav e Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { HeaderComponent } from './components/templates/header/header.component';
+import { FooterComponent } from './components/templates/footer/footer.component'
+import { NavComponent } from './components/templates/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CardFilmComponent } from './components/templates/card-film/card-film.component';
+import { ListFilmsComponent } from './views/list-films/list-films.component';
+import { SelectButtonComponent } from './components/templates/select-button/select-button.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    CardFilmComponent,
+    ListFilmsComponent,
+    SelectButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatButtonToggleModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/views/checkout/checkout.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Film } from '../list-films/film.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckoutService {
+  public baseUrl: string = 'http://localhost:3001';
+  public listFilms: Film[] = [];
+  private _priceHandler: number = 0;
+  public totalPrice: number = 0; 
+
+  getPrice(): number {
+    return this._priceHandler;
+  }
+
+  setPrice(value: number) {
+    this._priceHandler = value;
+  }
+
+  constructor(private httpClient: HttpClient) { 
+  }
+
+  getListFilms(): Observable<Film[]> {
+    return this.httpClient.get<Film[]>(this.baseUrl+'/films');
+  }
+
+  selectFilm() {
+    this.totalPrice += this.getPrice();
+  }
+
+  unselectFilm() {
+    this.totalPrice -= this.getPrice();
+    if (this.totalPrice < 0) {
+      this.totalPrice = 0;
+    }
+  }
+}
